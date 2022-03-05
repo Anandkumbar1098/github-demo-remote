bc17b4820ac2e8dfce265653fa358aec85dc097a
anand_kumbar <anandkumbar1098@gmail.com>
2022-03-05 10:05:38 +0530

    Added Header information

--- math.py
+++ math.py
@@ -1,15 +1,20 @@
+#Add  implementation
 def add(x,y):
     pass 
 
+#Substract  implementation
 def substract(x,y):
     pass
   
+#Multiply  implementation
 def multiply(x,y):
     pass
-
+    
+#Divide  implementation
 def divide(x,y):
     pass
  
+#Square  implementation
 def square(x):
     pass
 
